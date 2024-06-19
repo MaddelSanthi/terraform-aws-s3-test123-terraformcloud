9a09eb60e817d1e194eeaf6569ab83117f9e405c
MaddelSanthi <168314503+MaddelSanthi@users.noreply.github.com>
2024-06-19 09:23:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ terraform {
 provider "aws"{
 region="us-east-1"
 assume_role {
-    role_arn = "arn:aws:iam::853242284930:role/awsscalrintegration1"
+    role_arn = "arn:aws:iam::853242284930:role/ScalrIntegrationRole"
   }
 }
 resource "aws_s3_bucket" "my_bucket" {
