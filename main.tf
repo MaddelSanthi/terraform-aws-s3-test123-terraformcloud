e50636cba1dab782aa6430599d5137a7a4f8e956
MaddelSanthi <168314503+MaddelSanthi@users.noreply.github.com>
2024-06-25 14:25:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,21 +1,10 @@
 
 
-terraform {
-  backend "remote" {
-    hostname = "protran.scalr.io"
-    organization = "env-v0odelrmuhoqui60a"
-
-    workspaces {
-      name = "test123"
-    }
-  }
-}
-
 provider "aws"{
 region="us-east-1"
-#assume_role {
- #   role_arn = "arn:aws:iam::853242284930:role/ScalrIntegrationRole"
-  #}
+access_key = "AKIA4NKJUBOBJHVRBX4C"
+secret_key = "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"
+
 }
 resource "aws_s3_bucket" "my_bucket" {
  bucket = "my-tf-test-bucket-${var.unique_id}"
