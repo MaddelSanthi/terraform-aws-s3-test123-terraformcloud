2d86d9ad9e39f9a1cb7f030fecf3dd24dd07f33a
MaddelSanthi <168314503+MaddelSanthi@users.noreply.github.com>
2024-06-25 14:30:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ resource "aws_s3_bucket" "my_bucket" {
 variable "unique_id" {
   description = "A unique identifier for the bucket name"
   type        = string
-  default     = "20240618-1234"  # Replace with your unique identifier
+  default     = "20240618-7777"  # Replace with your unique identifier
 }
 
 
